a57351201d754b1a1a8b1cbf478489b2f6355f5f
tcw645 <tcw645@a20edde3-ce5a-ee48-9cd3-f837a35abfa0>
2008-05-22 14:19:28 +0000

    Removed Test Comment.
    git-svn-id: svn://fhca01@931 a20edde3-ce5a-ee48-9cd3-f837a35abfa0

--- WindowsApplication1/Form1.cs
+++ WindowsApplication1/Form1.cs
@@ -50,8 +50,7 @@ using System.Windows.Forms;
 // UI events HAVE CODE IN THEM WHICH IS NOT ABSTRACTED INTO FUNCTIONS AND WILL NEED INDIVIDUAL ATTNETION WHEN UPDATING
 // it is arranged by tab and then grouped by the groupbox in which it appears on the form
 // UI events set the values in the datastructure like classes
-// UI events call execution methods\
-// Test SVn line
+// UI events call execution methods
 
 
 
